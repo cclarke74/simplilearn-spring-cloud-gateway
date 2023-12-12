8f58d63805d75d19689c1ca4c72b033ccaa7242c
cclarke74 <clairc2@gmail.com>
2023-12-11 16:27:04 -0800

    first commit

---
+++ src/main/java/com/simplilearn/spring/SimplilearnSpringCloudGatewayApplication.java
@@ -0,0 +1,14 @@
+package com.simplilearn.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SimplilearnSpringCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplilearnSpringCloudGatewayApplication.class, args);
+	}
+
+}
